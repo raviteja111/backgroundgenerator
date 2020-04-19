44bf20e4a270acb023e2d19d8b36f717d7df1854
rtdasari <raviinfo44@gmail.com>
2020-04-19 14:47:34 +0530

    adding starting project

---
+++ html.py
@@ -0,0 +1,14 @@
+
+import webbrowser
+
+# f = open('helloworld.html','wb')
+
+# message = """<html>
+# <head></head>
+# <body><p>Hello World!</p></body>
+# </html>"""
+
+# f.write(message)
+# f.close()
+
+webbrowser.open_new_tab('New.html')
